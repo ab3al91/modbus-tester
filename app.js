7bec7d63441d1390ebe084e47329ae9a0366e932
Abzal Aiten <ayten.abzal91@gmail.com>
2018-12-26 19:57:59 +0600

    final

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const expressws = require('express-ws');
 const ModbusRTU = require("modbus-serial");
+var bodyParser = require('body-parser')
 const app = express()
 let ws = expressws(app);
 var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use(bodyParser.json())
 
 app.ws('/', function(ws, req) {
   console.log('!!!!!!!!socket connected');
@@ -13,28 +15,22 @@ app.ws('/', function(ws, req) {
 
 var wss = ws.getWss('/');
 
-var brodcast = (slave)=>{
-  wss.clients.forEach(function (client) {
+var brodcast = (d)=>{
+  wss.clients.forEach(function (wclient) {
     // console.log('Brodcast',msg);
-    if(slave){
         var json = {};
-        json[slave] = data[slave];
-        client.send(JSON.stringify(json));
-    }
-    else
-        client.send(JSON.stringify(data));
+        json['0'] = d;
+        json['isOpen'] = client.isOpen;
+        data = d;
+        wclient.send(JSON.stringify(json));
+ 
   });
 }
 
 
 
-var data = {"1":[],"2":[], "3":[],"4":[],"5":[],"6":[]};
-var queue = [];
+var data = [];
 //var writeG = 0;
-
-var slaveG;
-var regG;
-var valueG;
 
 
 app.disable('etag');
@@ -55,18 +51,95 @@ app.use(function (req, res, next) {
 });
 
 
-app.get('/write/:slave/:reg/:value', (req, res, next) => {
-    slaveG = Number(req.params.slave);
-    regG = Number(req.params.reg);
-    valueG = Number(req.params.value);
 
-    console.log('push',slaveG,regG,valueG)
 
-    queue.push([slaveG,regG,valueG,next]);
-    
-    
-    // next();
+
+// create an empty modbus client
+var interval;
+var clientParam={
+	ip:"87.255.213.153",
+	port:5002,
+	startReg:0,
+	lastReg:0,
+	type:1,
+	parity:0,
+	baudRate:0,
+	slaveId:0
+};
+var client = new ModbusRTU();
+// set timeout, if slave did not reply back
+client.setTimeout(10000);
+
+// client.connectTCP("87.255.213.153", { port: 5002 }, ()=> {
+// 	// start get value
+// 	console.log('modbus connected ' + client.isOpen);
+// 	//if(client.isOpen) getSlavesValue(slavesIdList);
+// 	if(client.isOpen) {
+// 		setInterval(function() {
+// 			client.readHoldingRegisters(0, 5, function(err, data) {
+// 			console.log(data.data);
+// 			});
+// 		}, 4000);
+// 	}
+// });
+
+app.post('/toggle', function (req, res) {
+
+
+	// open connection to a serial port
+	//client.connectRTU("/dev/ttyUSB0", { baudRate: 19200 }, ()=> {
+	/*client.connectRTUBuffered("/dev/ttyUSB0", { baudRate: 19200 }, ()=> {
+		// start get value
+		console.log('modbus connected ' + client.isOpen);
+		if(client.isOpen) getSlavesValue(slavesIdList);
+	});*/
+	clientParam = req.body
+
+	console.log('toggle ',clientParam);
+
+	res.setHeader('Content-Type', 'application/json');
+
+	if(client && client.isOpen){
+		client.close();
+		clearInterval(interval);
+		res.send(JSON.stringify({isOpen:false}));
+	}
+	else{
+		
+		client.connectTCP(clientParam.ip, { port: Number(clientParam.port) }).then(()=> {
+			// start get value
+			console.log('modbusTCP connected ' + client.isOpen,clientParam);
+			//if(client.isOpen) getSlavesValue(slavesIdList);
+			if(client.isOpen) {
+				readVals(client);
+				interval = setInterval(readVals, 5000);
+			}
+
+			res.send(JSON.stringify({isOpen:client.isOpen}));
+		}).catch(function(e) {
+	        console.log(e.message);
+	        res.send(JSON.stringify({isOpen:client.isOpen,error:e.message}));
+	    });
+	}
 })
+
+
+
+function readVals() {
+
+	// console.log(client.isOpen,clientParam);
+
+	if(client.isOpen){
+		client.readHoldingRegisters(Number(clientParam.startReg), Number(clientParam.lastReg)).then((d)=>{
+			console.log(d.data);
+			brodcast(d.data);
+		}).catch((err)=>{
+			console.log(err);
+		});
+	}
+ }
+
+
 
 app.get('*', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
@@ -74,160 +147,21 @@ app.get('*', function (req, res) {
 })
 
 
-// create an empty modbus client
-const client = new ModbusRTU();
-// open connection to a serial port
-//client.connectRTU("/dev/ttyUSB0", { baudRate: 19200 }, ()=> {
-/*client.connectRTUBuffered("/dev/ttyUSB0", { baudRate: 19200 }, ()=> {
-	// start get value
-	console.log('modbus connected ' + client.isOpen);
-	if(client.isOpen) getSlavesValue(slavesIdList);
-});*/
-client.connectTCP("87.255.213.153", { port: 5002 }, ()=> {
-	// start get value
-	console.log('modbus connected ' + client.isOpen);
-	//if(client.isOpen) getSlavesValue(slavesIdList);
-	if(client.isOpen) {
-		setInterval(function() {
-			client.readHoldingRegisters(0, 5, function(err, data) {
-			console.log(data.data);
-			});
-		}, 5000);
-	}
-});
+
+//const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 
+//const log = (txt) => process.stdout.write('\n'+txt);
 
-// set timeout, if slave did not reply back
-client.setTimeout(1000);
+// const clear = (txt) => {
+//     process.stdout.moveCursor(0, -6);
+//     process.stdout.cursorTo(0);
+//     process.stdout.clearScreenDown();
+// }
 
-// list of meter's id
-/*const slaveRegLength = [19, 7, 5, 4, 4, 7];
-const slavesIdList = [1, 2, 3, 4, 5, 6];
-
-var start = 0;
-
-const getSlavesValue = async (slaves) => {
-    try{
-        // get value of all meters
-        for(let slave of slaves) {
-
-            start = new Date().getTime();
-
-            while((a=queue.pop()) != null){
-                //await sleep(50);
-                await setSlaveValue(a[0], a[1], a[2]);
-                console.log('>>>>>>>>',a,tim());
-                // await sleep(50);
-                // await getSlaveValue(a[0]);
-                a[3]();
-            }
-
-            start = new Date().getTime();
-
-            var firstRegScan = await getFirstReg(slave);
-            // await sleep(50);
-            
-            // output value to console
-            if(firstRegScan[0] == 1){
-                var reg = await getSlaveValue(slave);
-                // await sleep(50);
-                log(slave+':'+reg+':'+tim());
-                await setFirstReg(slave);
-            }
-            else{
-                log(slave+':'+firstRegScan+':'+tim());
-            }
-
-            start = new Date().getTime();
-
-            await sleep(50);
-
-            // log('------------------------'+tim());
-	   }
-    } catch(e){
-        // if error, handle them here (it should not)
-        console.log(e)
-    } finally {
-    	//clear();
-        // after get all data from salve repeate it again
-        setImmediate(() => {
-            getSlavesValue(slavesIdList);
-        })
-    }
-}
-
-
-const getFirstReg = async (id) => {
-    try{
-        await client.setID(id);
-        let val = await client.readHoldingRegisters(0, 1);
-        //await sleep(50);
-        return val.data;
-    } catch(e){
-        console.log(e)
-        return -1
-    }
-}
-
-const getSlaveValue = async (id) => {
-    try {
-        // set ID of slave
-        await client.setID(id);
-        // read the 1 registers starting at address 0 (first register)
-        let reg = await client.readHoldingRegisters(0, slaveRegLength[id-1]);
-            reg = reg.data;
-
-        //await sleep(50);
-        data[id] = reg;
-        brodcast(id);
-        return reg;
-    } catch(e){
-        // if error return -1
-        console.log(e)
-        return -1
-    }
-}
-
-const setSlaveValue = async (id, register, value) => {
-    try {
-        // set ID of slave
-        await client.setID(id);
-        await client.writeRegister(register, value);
-        await client.writeRegister(0,1);
-        await sleep(50);
-    } catch(e){
-        
-        console.log(e)
-    }
-}
-
-const setFirstReg = async (id) => {
-    try {
-        // set ID of slave
-        await client.setID(id);
-        await client.writeRegister(0,0);
-        // await sleep(50);
-    } catch(e){
-        
-        console.log(e)
-    }
-}*/
-
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-
-const log = (txt) => process.stdout.write('\n'+txt);
-
-const clear = (txt) => {
-    process.stdout.moveCursor(0, -6);
-    process.stdout.cursorTo(0);
-    process.stdout.clearScreenDown();
-}
-
-const tim = ()=>{
-    return (new Date().getTime() - start) + 'ms';
-}
+// const tim = ()=>{
+//     return (new Date().getTime() - start) + 'ms';
+// }
 
 app.listen(port, () => console.log(`
            __         __ ___       __      __ 
